bb02f035c42269e70b404cd5de5e5e5792f4d559
Nguyen The Thuc <thucfami@gmail.com>
2015-12-01 20:34:01 +0700

    facebook api

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -21,6 +21,23 @@ AppAsset::register($this);
     <?php $this->head() ?>
 </head>
 <body>
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1502054410090394',
+      xfbml      : true,
+      version    : 'v2.5'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
 
 <?php $this->beginBody() ?>
     <div class="wrap">
