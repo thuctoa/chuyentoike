d8f6b5b6a2f02ff04ffd652bba2bc57cfbe42b88
Nguyen The Thuc <thucfami@gmail.com>
2016-01-12 21:53:48 +0700

    fb login

--- views/site/loginfacebook.php
+++ views/site/loginfacebook.php
@@ -70,7 +70,7 @@
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
     js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/vi_VN/sdk.js";
+    js.src = "//connect.facebook.net/en_US/sdk.js";
     fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));
 
