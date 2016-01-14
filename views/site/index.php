8dfce49dc3bcce1e96d520c6272d2b7a32733402
Nguyen The Thuc <thucfami@gmail.com>
2016-01-14 10:14:10 +0700

    anh dau bai

--- views/site/index.php
+++ views/site/index.php
@@ -58,6 +58,7 @@ $actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
                     <h1 class="text-center tieude-chinh">
                         <?=$baiviet['title']?>
                     </h1>
+                    <img src="/uploads/<?=$baiviet['img']?>" class="anhdaubai"/>
                     <?=$baiviet['description']?>
                 </div>
                 <div class="noidung-chinh">
