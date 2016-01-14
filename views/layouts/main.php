008115724a04430aa03385ed14663a9eb9810bfe
Nguyen The Thuc <thucfami@gmail.com>
2016-01-14 09:00:41 +0700

    the meta

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -16,6 +16,7 @@ AppAsset::register($this);
 <head>
     <meta charset="<?= Yii::$app->charset ?>"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta property="fb:app_id" content="1502054410090394" />
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
